9d85d5b156ceef5f4d21607c148148b7ab1d1184
dinushika096 <dinushika096@gmail.com>
2024-07-21 13:44:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "94724800886",
+  SUDO_NUMBERS : "94724800886",
+  OWNER_NAME: "Ｉｎｆｉｎｉ   Ｘ",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
